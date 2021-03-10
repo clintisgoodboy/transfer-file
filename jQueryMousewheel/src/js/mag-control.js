47d365d0e7b3b52cc040cfa1f89879dc7c2024e3
clint <>
2021-03-10 15:04:09 +0800

    ccc

--- jQueryMousewheel/src/js/mag-control.js
+++ jQueryMousewheel/src/js/mag-control.js
@@ -1,24 +1,29 @@
 (function (root, factory) {
   var name = 'MagnificentControl';
   if (typeof define === 'function' && define.amd) {
-    define(['jquery', './mag-analytics', 'jquery-bridget'], function (jQuery, MagnificentAnalytics) {
-        return (root[name] = factory(jQuery, MagnificentAnalytics));
+    define(['jquery', './mag-analytics', 'jquery-bridget'], function (
+      jQuery,
+      MagnificentAnalytics
+    ) {
+      return (root[name] = factory(jQuery, MagnificentAnalytics));
     });
   } else if (typeof exports === 'object') {
-    module.exports = factory(require('jquery'), require('./mag-analytics'), require('jquery-bridget'));
+    module.exports = factory(
+      require('jquery'),
+      require('./mag-analytics'),
+      require('jquery-bridget')
+    );
   } else {
     root[name] = factory(root.jQuery, root.MagnificentAnalytics);
   }
-}(this, function ($, MagnificentAnalytics) {
-
+})(this, function ($, MagnificentAnalytics) {
   var MagnificentControl = function (element, options) {
-    this.element = $( element );
-    this.options = $.extend( true, {}, this.options, options );
+    this.element = $(element);
+    this.options = $.extend(true, {}, this.options, options);
     this._init();
   };
 
   MagnificentControl.prototype._init = function () {
-
     var $el = this.element;
     var el = $el.get(0);
 
@@ -32,24 +37,28 @@
 
     $el.find('[mag-ctrl-zoom-by]').on('click', function () {
       var zoomBy = $.parseJSON($(this).attr('mag-ctrl-zoom-by'));
+      console.log(zoomBy);
       magInst.zoomBy(zoomBy);
+      console.log(magInst);
     });
 
-    $el.find('[mag-ctrl-move-by-x], [mag-ctrl-move-by-y]').on('click', function () {
-      var x = $(this).attr('mag-ctrl-move-by-x');
-      if (x) {
-        x = $.parseJSON(x);
-      }
-      var y = $(this).attr('mag-ctrl-move-by-y');
-      if (y) {
-        y = $.parseJSON(y);
-      }
-      var moveBy = {
-        x: x,
-        y: y
-      };
-      magInst.moveBy(moveBy);
-    });
+    $el
+      .find('[mag-ctrl-move-by-x], [mag-ctrl-move-by-y]')
+      .on('click', function () {
+        var x = $(this).attr('mag-ctrl-move-by-x');
+        if (x) {
+          x = $.parseJSON(x);
+        }
+        var y = $(this).attr('mag-ctrl-move-by-y');
+        if (y) {
+          y = $.parseJSON(y);
+        }
+        var moveBy = {
+          x: x,
+          y: y,
+        };
+        magInst.moveBy(moveBy);
+      });
 
     $el.find('[mag-ctrl-fullscreen]').on('click', function () {
       if (screenfull) {
@@ -64,13 +73,11 @@
     });
   };
 
-
   $.bridget('magCtrl', MagnificentControl);
-
 
   if (MagnificentAnalytics) {
     MagnificentAnalytics.track('mag-control.js');
   }
 
   return MagnificentControl;
-}));
+});
